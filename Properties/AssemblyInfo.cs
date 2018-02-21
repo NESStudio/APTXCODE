593e8a6597d65f2f23b9d9124569a5680bd0a5a2
Alex Liu <alex.liu.199911@gmail.com>
2018-02-21 13:26:01 +0800

    更新版本

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      内部版本号
 //      修订号
 //
-[assembly: AssemblyVersion("1.1.0.6")]
-[assembly: AssemblyFileVersion("1.1.0.6")]
+[assembly: AssemblyVersion("1.1.0.7")]
+[assembly: AssemblyFileVersion("1.1.0.7")]
 [assembly: NeutralResourcesLanguageAttribute("zh-CN")]
